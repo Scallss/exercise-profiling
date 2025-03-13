8b2ea2ec163b634b0573d8f2cc9b9184fb8b83a0
Scallss <pascalhafidz2005@gmail.com>
2025-03-13 10:10:10 +0700

    Refactor: joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -42,11 +42,17 @@ public class StudentService {
 
     public String joinStudentNames() {
         List<Student> students = studentRepository.findAll();
-        String result = "";
-        for (Student student : students) {
-            result += student.getName() + ", ";
+        if (students.isEmpty()) {
+            return "";
         }
-        return result.substring(0, result.length() - 2);
+
+        StringBuilder result = new StringBuilder();
+        for (Student student : students) {
+            result.append(student.getName()).append(", ");
+        }
+
+        result.setLength(result.length() - 2);
+        return result.toString();
     }
 }
 
